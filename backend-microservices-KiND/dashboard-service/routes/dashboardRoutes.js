35543913c1e3e5270be9b7469a6a728950db6d79
MdSourovAhmed <96908951+MdSourovAhmed@users.noreply.github.com>
2026-09-13 16:29:28 +0600

    Adding Client and Admin section

--- backend-microservices-KiND/dashboard-service/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getSummary } = require('../controllers/dashboardController');
-const { authClientUser, requireRole } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/admin/dashboard/summary', authClientUser, requireRole('staff'), getSummary);
-
-module.exports = router;
